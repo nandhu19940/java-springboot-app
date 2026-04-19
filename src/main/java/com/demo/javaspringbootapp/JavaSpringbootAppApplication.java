5d54d1fa0125376ae2883523cfd4ebe0ce0dcdf6
nandhu19940 <nandha008kumar@gmail.com>
2026-04-19 16:40:35 +0530

    Initial commit - Spring Boot app with hello endpoint

---
+++ src/main/java/com/demo/javaspringbootapp/JavaSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.demo.javaspringbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/javaspringbootapp/JavaSpringbootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.javaspringbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringbootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
